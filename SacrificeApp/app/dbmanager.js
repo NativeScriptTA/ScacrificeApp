af99a08442a900e461e9494f0d78387188c09e80
PavelNPavlov <pavelnapavlov@gmail.com>
2016-02-22 01:44:45 +0200

    update dbmanager.js for use with the new table

--- SacrificeApp/app/dbmanager.js
+++ SacrificeApp/app/dbmanager.js
@@ -37,6 +37,47 @@
 			  '")');
 		};
 
+		DBManager.prototype.insertItem = function(name, type) {
+			 db.execSQL('insert into Items (Name, Type) values ("' +
+			  name +
+			  '", "' +
+			  type +
+		};
+
+		DBManager.prototype.getAllItems = function(callback) {
+			db.all('select Name, Type from Items', function (err, loadedData) {
+				var result = [];
+		        if (err) {
+		            console.log(err);
+		        } else {
+		        	for(let i = 0; i < loadedData.length; i++) {
+		        		let data = {};
+		        		data.name = JloadedData[i].Name;
+		        		data.type = loadedData[i].Type;
+		        		result.push(data);
+		        	}
+		        }
+		        callback(result);
+	    	});
+		};
+
+		DBManager.prototype.getItemByName = function(name, callback) {
+			db.all('select Name, Type from Items where Name = "' + name + '"', function (err, loadedData) {
+				var result = [];
+		        if (err) {
+		            console.log(err);
+		        } else {
+		        	for(let i = 0; i < loadedData.length; i++) {
+		        		let data = {};
+								data.name = JloadedData[i].Name;
+		        		data.type = loadedData[i].Type;
+		        		result.push(data);
+		        	}
+		        }
+		        callback(result);
+	    	});
+		};
+
 		DBManager.prototype.getMagicInfoByName = function(name, callback) {
 			db.all('select MagicData, Degree, Source from magicsHistory where MagicName = "' + name + '"', function (err, loadedData) {
 				var result = [];
